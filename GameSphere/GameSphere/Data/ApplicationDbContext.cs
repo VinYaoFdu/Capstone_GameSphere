d4e809e7e02a0522b722f432ce211c9bef53743d
ShaoweiY <s.yao1@student.fdu.edu>
2023-08-03 01:39:29 -0700

    Day1: Initialization
    a) basic functions: register, login, create new posts, check posts' details.
    b) UI

---
+++ GameSphere/GameSphere/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using GameSphere.Models;
+
+namespace GameSphere.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<GameSphere.Models.Post> Post { get; set; } = default!;
+    }
+}
---
+++ GameSphere/GameSphere/Data/Migrations/20230712215842_GameSetUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GameSphere.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GameSetUp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Post",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Topic = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PostedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MessaAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Post", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Post");
+        }
+    }
+}
